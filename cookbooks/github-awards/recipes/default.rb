582cd88688d16f19282f3f4779416ea3bdea52c0
Vincent Daubry <vdaubry@gmail.com>
2016-01-22 20:11:21 +0100

    add data bags for user datas

--- cookbooks/github-awards/recipes/default.rb
+++ cookbooks/github-awards/recipes/default.rb
@@ -21,25 +21,17 @@ apt_package "ruby2.2 ruby2.2-dev build-essential zlib1g-dev libpq-dev nodejs"
 gem_package 'bundler'
 
 
-group 'ubuntu'
-user 'ubuntu' do
-  group 'ubuntu'
-  system true
-  shell '/bin/bash'
-end
-ssh_authorize_key 'ubuntu' do
-  key 'AAAAB3NzaC1yc2EAAAADAQABAAABAQC3AmTJMN5tpCljaddrj4KzlKRBLSr+Tyz7/ch4egpi0FxbfltJ/PQFEOpz4U2TZXFE8aehqtiGwcYLI25mEtV8tiridsy7LLwFE2yPR0oKEACfuidomCQSOr/3ifCwP8hOy2lPupPIwHZT+9TjQObgdv7sZNj3xEvlWVpvXPb50Opqp/tq5vSAsUb1vz8EO2ik/0wZt31i4HygQ4SKz4Yz+LM298kIlVx2nFyK6iPUWom47FMgiz2hMYBEi8r6uYDSAwWtUN36eP4w2QL72IZMeuYTo0tFJOzpPNXyvSH8kBqPHqIfpo2DU8Bh5R6lW59GUnL2rd5KnNNQJHmC83iB'
-  user 'ubuntu'
-end
-
-
-group 'deploy'
-user 'deploy' do
-  group 'deploy'
-  system true
-  shell '/bin/bash'
-end
-ssh_authorize_key 'deploy' do
-  key 'AAAAB3NzaC1yc2EAAAADAQABAAABAQC3AmTJMN5tpCljaddrj4KzlKRBLSr+Tyz7/ch4egpi0FxbfltJ/PQFEOpz4U2TZXFE8aehqtiGwcYLI25mEtV8tiridsy7LLwFE2yPR0oKEACfuidomCQSOr/3ifCwP8hOy2lPupPIwHZT+9TjQObgdv7sZNj3xEvlWVpvXPb50Opqp/tq5vSAsUb1vz8EO2ik/0wZt31i4HygQ4SKz4Yz+LM298kIlVx2nFyK6iPUWom47FMgiz2hMYBEi8r6uYDSAwWtUN36eP4w2QL72IZMeuYTo0tFJOzpPNXyvSH8kBqPHqIfpo2DU8Bh5R6lW59GUnL2rd5KnNNQJHmC83iB'
-  user 'deploy'
+users = data_bag_item('users', 'infos')
+users.delete('id')
+users.each do |name, infos|
+  group infos['group']
+  user infos['user'] do
+    group infos['group']
+    system true
+    shell '/bin/bash'
+  end
+  ssh_authorize_key infos['user'] do
+    key infos['key']
+    user infos['user']
+  end
 end
