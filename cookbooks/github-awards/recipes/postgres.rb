96850a8c4e45970a367c90324b4329fe4f65c6e2
Vincent Daubry <vdaubry@gmail.com>
2016-07-11 15:32:43 +0800

    install postgres client and server

--- cookbooks/github-awards/recipes/postgres.rb
+++ cookbooks/github-awards/recipes/postgres.rb
@@ -5,3 +5,18 @@
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 
 
+node.set['postgresql']['enable_pgdg_apt'] = true
+
+node.set['postgresql']['version'] = "9.4"
+
+node.set['postgresql']['setup_script'] = "postgresql94-setup"
+
+if node['postgresql']['enable_pgdg_apt']
+  node.set['postgresql']['dir'] = '/etc/postgresql/9.4/main'
+  node.set['postgresql']['server']['service_name'] = "postgresql"
+  node.set['postgresql']['server']['packages'] = ["postgresql-9.4"]
+  node.set['postgresql']['client']['packages'] = ["postgresql-client-9.4", "libpq-dev"]
+end
+
+include_recipe "postgresql::ruby"
+include_recipe "postgresql::server"
