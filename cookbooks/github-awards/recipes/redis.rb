d4b82b6cea1a4f57fd629914cffaac5171223add
Vincent Daubry <vdaubry@gmail.com>
2016-07-11 15:56:40 +0800

    configure redis

---
+++ cookbooks/github-awards/recipes/redis.rb
@@ -0,0 +1,9 @@
+#
+# Cookbook Name:: github-awards
+# Recipe:: redis
+#
+# Copyright (c) 2016 The Authors, All Rights Reserved.
+
+
+include_recipe 'redisio::default'
+include_recipe 'redisio::enable'
