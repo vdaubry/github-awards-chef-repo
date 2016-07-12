28eb4880485ff17bc58b2294558283e1ac43ff9a
Vincent Daubry <vdaubry@gmail.com>
2016-07-12 16:31:44 +0800

    create app folders

---
+++ cookbooks/github-awards/recipes/folders.rb
@@ -0,0 +1,17 @@
+#
+# Cookbook Name:: github-awards
+# Recipe:: folders
+#
+# Copyright (c) 2016 The Authors, All Rights Reserved.
+
+directory '/srv/www/githubawards/releases' do
+  owner 'deploy'
+  group 'deploy'
+  mode '0775'
+end
+
+directory '/srv/www/githubawards/shared' do
+  owner 'deploy'
+  group 'deploy'
+  mode '0775'
+end
