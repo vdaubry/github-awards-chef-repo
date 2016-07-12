33b9a7feac88b874955be9fc5e3da98cb183c8a6
Vincent Daubry <vdaubry@gmail.com>
2016-07-12 16:25:20 +0800

    add berskfile

--- cookbooks/github-awards/recipes/default.rb
+++ cookbooks/github-awards/recipes/default.rb
@@ -12,6 +12,7 @@ include_recipe 'github-awards::security_updates'
 include_recipe 'github-awards::swap'
 include_recipe 'github-awards::postgres'
 include_recipe 'github-awards::redis'
+include_recipe 'github-awards::nginx'
 
 apt_repository "ruby" do
   uri "http://ppa.launchpad.net/brightbox/ruby-ng/ubuntu"
---
+++ cookbooks/github-awards/recipes/nginx.rb
@@ -0,0 +1,7 @@
+#
+# Cookbook Name:: github-awards
+# Recipe:: nginx
+#
+# Copyright (c) 2016 The Authors, All Rights Reserved.
+
+include_recipe "nginx"
